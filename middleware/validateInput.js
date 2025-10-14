52f7bd7d1bf71061356fa10950f8167ab2902f87
lehlogonolo-alt <hlogisj@gmail.com>
2025-10-14 19:26:55 +0200

    Updated validateInput.js

--- middleware/validateInput.js
+++ middleware/validateInput.js
@@ -1,14 +1,51 @@
 const { body } = require('express-validator');
 
+// 🔐 Customer Registration Validation
 exports.registerValidation = [
-  body('fullName').matches(/^[A-Za-z\s]{2,50}$/),
-  body('idNumber').matches(/^\d{13}$/),
-  body('accountNumber').matches(/^\d{10}$/),
-  body('password').matches(/^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/)
+  body('fullName')
+    .matches(/^[A-Za-z\s]{2,50}$/)
+    .withMessage('Full name must be 2–50 letters'),
+
+  body('idNumber')
+    .matches(/^\d{13}$/)
+    .withMessage('ID number must be 13 digits'),
+
+  body('accountNumber')
+    .matches(/^\d{10}$/)
+    .withMessage('Account number must be 10 digits'),
+
+  body('password')
+    .matches(/^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/)
+    .withMessage('Password must be 8+ chars, include uppercase, number, and special character')
 ];
 
+// 🔐 Customer Login Validation
 exports.loginValidation = [
-  body('username').notEmpty(),
-  body('accountNumber').matches(/^\d{10}$/),
-  body('password').notEmpty()
+  body('username')
+    .notEmpty()
+    .withMessage('Username is required'),
+
+  body('accountNumber')
+    .matches(/^\d{10}$/)
+    .withMessage('Account number must be 10 digits'),
+
+  body('password')
+    .notEmpty()
+    .withMessage('Password is required')
 ];
+
+// 🔐 Employee Login Validation
+exports.employeeLoginValidation = [
+  body('username')
+    .isAlphanumeric()
+    .withMessage('Username must be alphanumeric')
+    .notEmpty()
+    .withMessage('Username is required'),
+
+  body('password')
+    .isString()
+    .withMessage('Password must be a string')
+    .notEmpty()
+    .withMessage('Password is required')
+];
+
